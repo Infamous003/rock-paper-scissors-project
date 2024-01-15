51bc51bda9c821b7a0dd0b66e356f53da46ddfe0
Infamous003 <arfathsyed0312@gmail.com>
2024-01-15 16:04:04 +0530

    Changed my approach a little. Added showMove() to display player & computer move. playGame() now takes in two arguments instead of one

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-function chosenOption(option) {
-  //This code is for display what the player has chosen
-  let showPlayerOptionElement = document.querySelector('.player');
-  showPlayerOptionElement.innerText = 'You: '+ option;
-  //This code is for displaying what the computer has chosen
-  let computerChoice = getComputerDecision();
-  console.log(computerChoice);
-  let showComputerOptionElement = document.querySelector('.computer');
-  showComputerOptionElement.innerText = 'Computer: ' + computerChoice;
-  return computerChoice;
+//     This function returns the player move     //
+function getPlayerMove(playerMove) {
+  return playerMove;
 }
 
-function getComputerDecision() {
+
+//     This function returns the computer move     //
+function getComputerMove() {
   let computerChoice = Math.random();
-  console.log(computerChoice);
+
+  //console.log(computerChoice);
   if(computerChoice > 0 && computerChoice <= 0.3) {
     return 'rock';
   }else if (computerChoice > 0.3 && computerChoice <= 0.6) {
@@ -22,57 +18,64 @@ function getComputerDecision() {
   }
 }
 
-function playGame(playerChoice) {
-  const computerMove = chosenOption(playerChoice);
-  let resultElement = document.querySelector('.js-result');
-  console.log('Player choice is ' + playerChoice);
-  console.log(computerMove);
 
-  if(playerChoice === 'rock') {
-    if(computerMove === 'rock') {
-      resultElement.innerText = 'Draw';
-    }else if (computerMove === 'paper') {
-      resultElement.innerText = 'You lose';
-    }
-    else if (computerMove === 'scissors') {
-      resultElement.innerText = 'You win';
-    }
-  }else if(playerChoice === 'paper') {
-    if(computerMove === 'rock') {
-      resultElement.innerText = 'You win';
-    }else if (computerMove === 'paper') {
-      resultElement.innerText = 'Draw';
-    }
-    else if (computerMove === 'scissors') {
-      resultElement.innerText = 'You lose';
-    }
-  }else if(playerChoice === 'scissors') {
-    if(computerMove === 'rock') {
-      resultElement.innerText = 'You lose';
-    }else if (computerMove === 'paper') {
-      resultElement.innerText = 'You win';
-    }
-    else if (computerMove === 'scissors') {
-      resultElement.innerText = 'Draw';
-    }
-  }
+//     This function displays the player move & the computer move     //
+function showMove(playerMove, computerMove) {
+  let showPlayerMoveEle = document.querySelector('.player');
+  let showComputerMoveEle = document.querySelector('.computer');
 
-  //----SAME THING WITH SWITCH CASE STATEMENTS-------
-  
+  showPlayerMoveEle.innerText = 'You: ' + playerMove;
+  showComputerMoveEle.innerText = 'Computer: ' + computerMove;
 }
 
 
+//     This function plays the game     //
+function playGame(playerMove, computerMove) {
+  showMove(playerMove, computerMove);
 
+  let resultElement = document.querySelector('.js-result');
+  
+  if(playerMove === computerMove) {
+    resultElement.innerHTML = 'Draw';
+  }else{
+    if(playerMove === 'rock') {
+      if(computerMove === 'paper'){
+        resultElement.innerText = 'You lose';
+      }else {
+        resultElement.innerText = 'You win';
+      }
+    }else if(playerMove === 'paper'){
+      if(computerMove === 'rock'){
+        resultElement.innerText = 'You win';
+      }else{
+        resultElement.innerText = 'You lose';
+      }
+    }else if(playerMove === 'scissors') {
+      if(computerMove === 'rock'){
+        resultElement.innerText = 'You lose';
+      }else {
+        resultElement.innerText = 'You win';
+      }
+    }
+  }
+}
 
-/*
-------------------------------------CHANGE OF PLANS--------------------------
-*/
+/*                      My first approach to do solve this project                                                */
+/*    In this approach, I                                               */
 
-// function getPlayerMove(playerMove) {
-//   return playerMove;
+// function chosenOption(option) {
+//   //This code is for display what the player has chosen
+//   let showPlayerOptionElement = document.querySelector('.player');
+//   showPlayerOptionElement.innerText = 'You: '+ option;
+//   //This code is for displaying what the computer has chosen
+//   let computerChoice = getComputerDecision();
+//   console.log(computerChoice);
+//   let showComputerOptionElement = document.querySelector('.computer');
+//   showComputerOptionElement.innerText = 'Computer: ' + computerChoice;
+//   return computerChoice;
 // }
 
-// function getComputerMove(computerMOve) {
+// function getComputerDecision() {
 //   let computerChoice = Math.random();
 //   console.log(computerChoice);
 //   if(computerChoice > 0 && computerChoice <= 0.3) {
@@ -84,15 +87,38 @@ function playGame(playerChoice) {
 //   }
 // }
 
-// function playGame(playerMove, computerMove) {
+// function playGame(playerChoice) {
+//   const computerMove = chosenOption(playerChoice);
 //   let resultElement = document.querySelector('.js-result');
-//   if(playerMove === computerMove) {
-//     resultElement.innerHTML = 'Draw';
-//   }else{
-//     if(playerMove === 'rock') {
-//       if(computerMove === 'paper'){
-//         resultElement.innerT
-//       }
+//   console.log('Player choice is ' + playerChoice);
+//   console.log(computerMove);
+
+//   if(playerChoice === 'rock') {
+//     if(computerMove === 'rock') {
+//       resultElement.innerText = 'Draw';
+//     }else if (computerMove === 'paper') {
+//       resultElement.innerText = 'You lose';
+//     }
+//     else if (computerMove === 'scissors') {
+//       resultElement.innerText = 'You win';
+//     }
+//   }else if(playerChoice === 'paper') {
+//     if(computerMove === 'rock') {
+//       resultElement.innerText = 'You win';
+//     }else if (computerMove === 'paper') {
+//       resultElement.innerText = 'Draw';
+//     }
+//     else if (computerMove === 'scissors') {
+//       resultElement.innerText = 'You lose';
+//     }
+//   }else if(playerChoice === 'scissors') {
+//     if(computerMove === 'rock') {
+//       resultElement.innerText = 'You lose';
+//     }else if (computerMove === 'paper') {
+//       resultElement.innerText = 'You win';
+//     }
+//     else if (computerMove === 'scissors') {
+//       resultElement.innerText = 'Draw';
 //     }
 //   }
 // }
